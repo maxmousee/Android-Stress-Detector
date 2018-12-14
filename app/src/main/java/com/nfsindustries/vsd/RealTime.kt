4ecf235f07c6d85af25c9b53da0152e1d6494767
Natan Facchin <n.facchin@sig.eu>
2018-12-14 11:51:15 +0100

    Added audio record permission request

--- app/src/main/java/com/nfsindustries/vsd/RealTime.kt
+++ app/src/main/java/com/nfsindustries/vsd/RealTime.kt
@@ -1,14 +1,39 @@
 package com.nfsindustries.vsd
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_real_time.*
 
 class RealTime : AppCompatActivity() {
+    private val REQUEST_RECORD_AUDIO_PERMISSION = 200
+
+    // Requesting permission to RECORD_AUDIO
+    private var permissionToRecordAccepted = false
+    private var permissions: Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        permissionToRecordAccepted = if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
+            grantResults[0] == PackageManager.PERMISSION_GRANTED
+        } else {
+            false
+        }
+        if (!permissionToRecordAccepted) finish()
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_real_time)
+
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION)
 
         freq_tv.text = "Done"
     }
