d679a3a5539ea8b43a31dcba6c7ed68c148c20c1
Natan Facchin <maxmousee@gmail.com>
2019-01-05 18:51:14 +0100

    Update AudioProcessor.kt

--- app/src/main/java/com/nfsindustries/vsd/AudioProcessor.kt
+++ app/src/main/java/com/nfsindustries/vsd/AudioProcessor.kt
@@ -42,18 +42,4 @@ class AudioProcessor() {
     fun toDouble(bytes: ByteArray): Double {
         return ByteBuffer.wrap(bytes).double
     }
-
-    /**
-     * A native method that is implemented by the 'vsd' native library,
-     * which is packaged with this application.
-     */
-    external fun processAudio(array: Array<Double>): Double
-
-    companion object {
-
-        // Used to load the 'vsd' native cpp library on application startup.
-        init {
-            System.loadLibrary("vsd")
-        }
-    }
 }
