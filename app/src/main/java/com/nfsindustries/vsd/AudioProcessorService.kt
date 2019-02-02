d83e6eb2ec53999516494d1c447e56b1d938aff6
Natan Facchin <maxmousee@gmail.com>
2019-02-02 21:39:22 +0100

    Removing dead code

--- app/src/main/java/com/nfsindustries/vsd/AudioProcessorService.kt
+++ app/src/main/java/com/nfsindustries/vsd/AudioProcessorService.kt
@@ -3,17 +3,14 @@ package com.nfsindustries.vsd
 import android.app.Service
 import android.content.Intent
 import android.media.AudioFormat
-import android.os.Handler
 import android.os.IBinder
-import android.os.Looper
-import android.os.Message
 import java.util.concurrent.Executors
 
 const val RECORDER_SAMPLE_RATE = 8000
 const val RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_MONO
 const val RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_FLOAT
 
-class AudioProcessorService() : Service() {
+class AudioProcessorService : Service() {
 
     private var mBinder: IBinder? = null
     private val micCaptureRunnable = MicCaptureRunnable()
@@ -31,14 +28,6 @@ class AudioProcessorService() : Service() {
     override fun onDestroy() {
         super.onDestroy()
         executorService.shutdownNow()
-    }
-
-    private val mHandler: Handler = object : Handler(Looper.getMainLooper()) {
-        override fun handleMessage(inputMessage: Message) {
-            if (Looper.getMainLooper().equals(Looper.myLooper())) {
-//                Log.d(LOG_TAG, "HI")
-            }
-        }
     }
 
 }
--- app/src/main/java/com/nfsindustries/vsd/MicCaptureRunnable.kt
+++ app/src/main/java/com/nfsindustries/vsd/MicCaptureRunnable.kt
@@ -1,21 +1,23 @@
 package com.nfsindustries.vsd
 
 import android.media.AudioRecord
-import android.media.MediaRecorder
+import android.media.AudioRecord.READ_BLOCKING
+import android.media.MediaRecorder.AudioSource.MIC
+import android.os.Process.THREAD_PRIORITY_BACKGROUND
 import android.util.Log
 
-class MicCaptureRunnable() : Runnable {
+class MicCaptureRunnable : Runnable {
 
     private val frequencyStringConverter = FrequencyConverter()
     private var audioDoubleArray = DoubleArray(RECORDER_SAMPLE_RATE)
     private var audioData = FloatArray(RECORDER_SAMPLE_RATE)
-    private val recorder = AudioRecord(MediaRecorder.AudioSource.MIC, RECORDER_SAMPLE_RATE, RECORDER_CHANNELS, RECORDER_AUDIO_ENCODING, RECORDER_SAMPLE_RATE)
+    private val recorder = AudioRecord(MIC, RECORDER_SAMPLE_RATE, RECORDER_CHANNELS, RECORDER_AUDIO_ENCODING, RECORDER_SAMPLE_RATE)
     private val vsd = VSDJNI()
     private var stressFrequency = 0.0
 
     private fun readAudioBuffer() {
         // gets the voice output from microphone to byte format
-        recorder.read(audioData, 0, RECORDER_SAMPLE_RATE, AudioRecord.READ_BLOCKING)
+        recorder.read(audioData, 0, RECORDER_SAMPLE_RATE, READ_BLOCKING)
         var index = 0
         for (value in audioData) {
             audioDoubleArray[index] = value.toDouble()
@@ -23,11 +25,12 @@ class MicCaptureRunnable() : Runnable {
         }
         stressFrequency = vsd.processAudio(audioDoubleArray)
         val formattedString = frequencyStringConverter.convertStressFrequencyFormattedString(stressFrequency)
-        Log.d(LOG_TAG, stressFrequency.toString() + " Hz")
+        val color = frequencyStringConverter.convertBackgroundColor(stressFrequency)
+        if(stressFrequency > 0) Log.d(LOG_TAG, "$stressFrequency Hz")
     }
 
     override fun run() {
-        android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_BACKGROUND)
+        android.os.Process.setThreadPriority(THREAD_PRIORITY_BACKGROUND)
         while (true) {
             readAudioBuffer()
         }
